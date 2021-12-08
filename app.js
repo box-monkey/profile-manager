582c900c3856017bae7e8afe7c53fce0eb7aed49
box-monkey <jr.gasparin15@gmail.com>
2021-12-08 12:25:35 -0500

    added inquirer module

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-// require fs is needed to connect and make use of its functions
-const fs = require('fs');
-// this will call html generate page function from the page-template file
-const generatePage = require('./src/page-template.js');
+// require will call the inquirer module to use its functions
+const inquirer = require('inquirer');
+// // require fs is needed to connect and make use of its functions
+// const fs = require('fs');
+// // this will call html generate page function from the page-template file
+// const generatePage = require('./src/page-template.js');
 
-// profiledataargs holds the users commands 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// these will extract and store 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'what is your name?'
+  }
+])
+.then(answers => console.log(answers));
 
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
-
-  console.log('portfolio complete! Check out the index.html to see the output!')
-})
+//   console.log('portfolio complete! Check out the index.html to see the output!')
+// })
